a857421ce4a1dd100ec52e409624a8d0b3dbc264
annalhq <155549764+annalhq@users.noreply.github.com>
2024-08-26 02:54:10 +0530

    sssp &  mazeAlgo added

---
+++ app/sssp/posttest/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const postTestPage = () => {
+  return (
+    <div>postTestPage</div>
+  )
+}
+
+export default postTestPage
---
+++ app/sssp/visualization/page.tsx
@@ -0,0 +1,14 @@
+// "use client"; // Add this line at the top
+
+// import React from 'react';
+// import PathfindingVisualizer from '@/algorithms/sssp/pathfindingVisualizer/pathfindingVisualizer';
+
+// const visualizationPage = () => {
+//   return (
+//     <div>
+//       <PathfindingVisualizer />
+//     </div>
+//   );
+// };
+
+// export default visualizationPage; 
