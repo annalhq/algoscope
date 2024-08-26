1af0801cdbb522d6f5a8e8b9e2e57ac193c0e48c
annalhq <155549764+annalhq@users.noreply.github.com>
2024-08-26 13:54:38 +0530

    sssp intro added

--- app/sssp/page.tsx
+++ app/sssp/page.tsx
@@ -1,21 +1,47 @@
 // app/algorithms/index.tsx
 import React from 'react';
+import 'katex/dist/katex.min.css';
+import Latex from 'react-latex-next';
 
 export default function intro() {
   return (
     <div className="container mx-auto p-4">
       <header className="mb-8">
-        <h1 className="text-4xl font-bold text-center">Algorithms Page</h1>
-        <p className="text-center text-gray-600">Learn about various algorithms and their complexities.</p>
+        <h1 className="text-4xl font-bold text-center">Single Source Shortest Path</h1>
+        <p className="text-center text-gray-600">Algorithms to find the shortest between two nodes.</p>
       </header>
 
       <section className="mb-8">
         <h2 className="text-2xl font-semibold mb-4">Introduction</h2>
         <p className="text-gray-700">
-          In this section, we introduce the algorithm, explaining its purpose and how it works.
-        </p>
+        Given a directed graph <Latex>$G = (V,E)$</Latex>, with non-negative costs on each edge, and a selected source node v in V, for all w in V, find the cost of the least cost path from v to w.
+
+        The cost of a path is simply the sum of the costs on the edges traversed by the path.
+
+        This problem is a general case of the more common subproblem, in which we seek the least cost path from v to a particular w in V. In the general case, this subproblem is no easier to solve than the SSSP problem.        </p>
       </section>
 
+      <section className="mb-8">
+      <h2 className="text-2xl font-semibold mb-4">Dijkstra's Algorithms</h2>
+      <p className="text-gray-700">
+
+      Suppose we are given a weighted directed graph  <Latex> $G = (V, E, w)$ </Latex> with two special
+      vertices, and we want to find the shortest path from a source vertex <Latex> $s$ </Latex> to a target
+      vertex  <Latex> $t$ </Latex>. That is, we want to find the directed path P starting at s and ending
+      at t that minimizes the function:
+      <Latex> {'$$w(P) := \\displaystyle \\sum_{u \\rightarrow v \\in P} w(u \\rightarrow v) $$'} </Latex>
+      <br />
+      Dijkstra's algorithm is a greedy algorithm for the SSSP problem.
+
+      A "greedy" algorithm always makes the locally optimal choice under the assumption that this will lead to an optimal solution overall.
+      Example: in making change using the fewest number of coins, always start with the largest coin possible.
+      Data structures used by Dijkstra's algorithm include:
+
+      a cost matrix C, where C[i,j] is the weight on the edge connecting node i to node j. If there is no such edge, <Latex>$C[i,j] = \infty$ </Latex>
+      a set of nodes S, containing all the nodes whose shortest path from the source node is known. Initially, S contains only the source node.
+      a distance vector D, where D[i] contains the cost of the shortest path (so far) from the source node to node i, using only those nodes in S as intermediaries.
+</p>
+      </section>
       <section className="mb-8">
         <h2 className="text-2xl font-semibold mb-4">Complexity</h2>
         <p className="text-gray-700">
@@ -31,7 +57,7 @@ export default function intro() {
         <div className="relative" style={{ paddingBottom: '56.25%' /* 16:9 aspect ratio */ }}>
           <iframe
             className="absolute top-0 left-0 w-full h-full"
-            src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+            src="https://www.youtube.com/embed/Aa2sqUhIn-E"
             title="YouTube video"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
